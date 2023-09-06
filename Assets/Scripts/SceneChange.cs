0121b9c13da1766c2dd4cc95a37a71b0d009e98c
deadheart123 <shreyas2799@gmail.com>
2023-09-06 14:53:08 +0100

    Final Prototype

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,14 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public static SceneChange instance;
 
-    public void OnSelectGalleryFreeLook()
+    public void UOFreelook()
     {
         SceneManager.LoadScene(1);
+        Cursor.visible = false;
     }
 
-    public void OnSelectGalleryCinematic()
+    public void UOCinematic()
     {
         SceneManager.LoadScene(2);
+        Cursor.visible = false;
+    }
+    public void OFreelook()
+    {
+        SceneManager.LoadScene(3);
+        Cursor.visible = false;
+    }
+    public void OCinematic()
+    {
+        SceneManager.LoadScene(4);
+        Cursor.visible = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            MainMenu();
+        }
     }
 }
